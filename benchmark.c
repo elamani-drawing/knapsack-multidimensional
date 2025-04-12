0f35b54f867cd1d7f07da800a0dfd324a3eed5aa
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-04-12 04:52:23 +0200

    [ADD] - vns_gloutonne_vs_aleatoire, export_csv

--- benchmark.c
+++ benchmark.c
@@ -5,9 +5,10 @@ double get_cpu_time()
     return (double)clock() / CLOCKS_PER_SEC;
 }
 
-ResultEntry run_experiment(const KnapsackInstance *instance, KnapsackSolution *(*initialization_function)(const KnapsackInstance *), int temps_max)
+ResultEntry run_experiment(const KnapsackInstance *instance, KnapsackSolution *(*initialization_function)(const KnapsackInstance *), int temps_max, const char *filename)
 {
-    ResultEntry result;
+    ResultEntry result = {"", 0.0, 0.0, 0,"vnc", 0, 0.0, 0, 0, 0};
+    
     double start_time = get_cpu_time();
     KnapsackSolution *solution = initialization_function(instance);
     variable_neighborhood_search(solution, instance, 100, 2, temps_max);
@@ -16,13 +17,16 @@ ResultEntry run_experiment(const KnapsackInstance *instance, KnapsackSolution *(
     result.value = solution->Z;
     result.time = end_time - start_time;
     result.length = solution_length(solution, instance);
+    strcpy(result.filename, filename);
 
     free_solution(solution);
     return result;
 }
 
-ResultEntry run_genetic_algorithm(const KnapsackInstance *instance, int population_size, int generations, double mutation_rate, int temps_max) {
-    ResultEntry result;
+ResultEntry run_genetic_algorithm(const KnapsackInstance *instance, int population_size, int generations, double mutation_rate, int temps_max, const char *filename) {
+
+    ResultEntry result = {"", 0.0, 0.0, 0,"genetic", 0, 0.0, 0, 0, 0};
+
     double start_time = get_cpu_time();
     KnapsackSolution *solution = genetic_algorithm(instance, population_size, generations, mutation_rate, temps_max);
     double end_time = get_cpu_time();
@@ -30,13 +34,16 @@ ResultEntry run_genetic_algorithm(const KnapsackInstance *instance, int populati
     result.value = solution->Z;
     result.time = end_time - start_time;
     result.length = solution_length(solution, instance);
+    strcpy(result.filename, filename);
 
     free_solution(solution);
     return result;
 }
 
-ResultEntry run_hybrid_algorithm(const KnapsackInstance *instance, int population_size, int generations, double mutation_rate, int vns_iterations, int k, int temps_max) {
-    ResultEntry result;
+ResultEntry run_hybrid_algorithm(const KnapsackInstance *instance, int population_size, int generations, double mutation_rate, int vns_iterations, int k, int temps_max, const char *filename) {
+
+    ResultEntry result = {"", 0.0, 0.0, 0,"hybrid", 0, 0.0, 0, 0, 0};
+
     double start_time = get_cpu_time();
     KnapsackSolution *solution = hybrid_GA_VNS(instance, population_size, generations, mutation_rate, vns_iterations, k, temps_max);
     double end_time = get_cpu_time();
@@ -44,24 +51,27 @@ ResultEntry run_hybrid_algorithm(const KnapsackInstance *instance, int populatio
     result.value = solution->Z;
     result.time = end_time - start_time;
     result.length = solution_length(solution, instance);
+    strcpy(result.filename, filename);
 
     free_solution(solution);
     return result;
 }
 
 
-ExperimentalResultsKSM run_all_experiments(const KnapsackInstance *instance, int temps_max)
+ExperimentalResultsKSM run_all_experiments(const KnapsackInstance *instance, int temps_max, const char *filename)
 {
+    // principalement utiliser pour le main()
     ExperimentalResultsKSM results;
-    results.greedy_vns = run_experiment(instance, greedy_initial_solution, temps_max);
-    results.random_vns = run_experiment(instance, random_initial_solution, temps_max);
+    results.greedy_vns = run_experiment(instance, greedy_initial_solution, temps_max, filename);
+    results.random_vns = run_experiment(instance, random_initial_solution, temps_max, filename);
 
-    results.genetic = run_genetic_algorithm(instance, 50, 100, 0.05, temps_max);
-    results.hybrid = run_hybrid_algorithm(instance, 50, 100, 0.05, 100, 2, temps_max);
+    results.genetic = run_genetic_algorithm(instance, 50, 100, 0.05, temps_max, filename);
+    results.hybrid = run_hybrid_algorithm(instance, 50, 100, 0.05, 100, 2, temps_max, filename);
     return results;
 }
 void print_results_table(const ExperimentalResultsKSM *results)
-{
+{  
+    // principalement utiliser dans le main()
     // Ligne de séparation complète avec la colonne Tailles
     printf("+------------------------------+-------------------------+-------------------------+----------+\n");
 
@@ -85,6 +95,133 @@ void print_results_table(const ExperimentalResultsKSM *results)
     // Ligne de séparation finale
     printf("+------------------------------+-------------------------+-------------------------+----------+\n");
 }
+
+int basic_test(int is_directory_mode, const char *path_instance, int temps_max)
+{
+    if (is_directory_mode)
+    {
+        DIR *dir = opendir(path_instance);
+        if (!dir)
+        {
+            perror("Erreur lors de l'ouverture du répertoire");
+            return 1;
+        }
+        struct dirent *entry;
+        while ((entry = readdir(dir)) != NULL)
+        {
+            char full_path[1024];
+            snprintf(full_path, sizeof(full_path), "%s/%s", path_instance, entry->d_name);
+
+            struct stat st;
+            if (stat(full_path, &st) == 0 && S_ISREG(st.st_mode))
+            {
+                printf("\n\n=== Instance : %s ===\n", entry->d_name);
+                KnapsackInstance ksInstance;
+                read_knapsack_file(full_path, &ksInstance);
+
+                ExperimentalResultsKSM results = run_all_experiments(&ksInstance, temps_max, entry->d_name);
+                print_results_table(&results);
+            }
+        }
+        closedir(dir);
+    }
+    else
+    {
+        KnapsackInstance ksInstance;
+        read_knapsack_file(path_instance, &ksInstance);
+
+        ExperimentalResultsKSM results = run_all_experiments(&ksInstance, temps_max, path_instance);
+        print_results_table(&results);
+    }
+
+    return 0;
+}
+
+void export_csv(ResultEntry *results, int data_index, const char *filename)
+{
+    FILE *file = fopen(filename, "w");
+    if (!file)
+    {
+        perror("Erreur d'ouverture du fichier CSV");
+        return;
+    }
+
+    // Écriture de l'en-tête
+    fprintf(file, "filename,value,time,length,type,pop_size,mutation_rate,generations,vns_iterations,k\n");
+
+    // Parcours du tableau de ResultEntry et écriture des données
+    for (int i = 0; i < data_index; i++) {
+        fprintf(file, "\"%s\",%lf,%lf,%d,\"%s\",%d,%lf,%d,%d,%d\n", 
+                results[i].filename,
+                results[i].value,
+                results[i].time,
+                results[i].length,
+                results[i].type,
+                results[i].pop_size,
+                results[i].mutation_rate,
+                results[i].generations,
+                results[i].vns_iterations,
+                results[i].k);
+    }
+
+    // Fermeture du fichier
+    fclose(file);
+    printf("Données exportées avec succès dans 'results.csv'.\n");
+}
+
+int vns_gloutonne_vs_aleatoire(const char *repertoire, int temps_max)
+{
+    printf("Test vns_gloutonne_vs_aleatoire.\n");
+    // Ouvrir le répertoire
+    DIR *dir = opendir(repertoire);
+    if (!dir)
+    {
+        perror("Erreur lors de l'ouverture du répertoire");
+        return 1;
+    }
+
+    struct dirent *entry;
+    // Tableau pour stocker les résultats à exporter
+    ResultEntry results[SIZE_RESULT_BUFFER];
+    int data_index = 0;
+
+    // Lire les fichiers du répertoire
+    while ((entry = readdir(dir)) != NULL)
+    {
+        char full_path[1024];
+        snprintf(full_path, sizeof(full_path), "%s/%s", repertoire, entry->d_name);
+
+        struct stat st;
+        // Vérifier si c'est un fichier régulier
+        if (stat(full_path, &st) == 0 && S_ISREG(st.st_mode))
+        {
+            printf("\n\n=== Instance : %s ===\n", entry->d_name);
+            KnapsackInstance ksInstance;
+            read_knapsack_file(full_path, &ksInstance);
+
+            // Exécuter les expériences VNS Gloutonne et VNS Aléatoire
+            ResultEntry greedy_result = run_experiment(&ksInstance, greedy_initial_solution, temps_max, entry->d_name);
+            greedy_result.type = "vns_gloutonne";
+            ResultEntry random_result = run_experiment(&ksInstance, random_initial_solution, temps_max, entry->d_name);
+            random_result.type = "vns_aleatoire";
+
+            // Sauvegarder les résultats dans le tableau
+            results[data_index] = greedy_result;
+            results[data_index + 1] = random_result;
+            data_index += 2;
+        }
+    }
+
+    closedir(dir);
+
+    export_csv(results, data_index, "./benchmark/vnc_gloutonne_vs_aleatoire.csv");
+
+    printf("Export des résultats terminé.\n");
+    return 0;
+}
+
+
+
 int main(int argc, char *argv[])
 {
     if (argc < 3)
@@ -115,41 +252,10 @@ int main(int argc, char *argv[])
     }
     int temps_max = atof(argv[is_directory_mode ? 3 : 2]);
 
-    if (is_directory_mode)
-    {
-        DIR *dir = opendir(path_instance);
-        if (!dir)
-        {
-            perror("Erreur lors de l'ouverture du répertoire");
-            return 1;
-        }
-        struct dirent *entry;
-        while ((entry = readdir(dir)) != NULL)
-        {
-            char full_path[1024];
-            snprintf(full_path, sizeof(full_path), "%s/%s", path_instance, entry->d_name);
 
-            struct stat st;
-            if (stat(full_path, &st) == 0 && S_ISREG(st.st_mode))
-            {
-                printf("\n\n=== Instance : %s ===\n", entry->d_name);
-                KnapsackInstance ksInstance;
-                read_knapsack_file(full_path, &ksInstance);
+    // return basic_test(is_directory_mode, path_instance, temps_max);
 
-                ExperimentalResultsKSM results = run_all_experiments(&ksInstance, temps_max);
-                print_results_table(&results);
-            }
-        }
-        closedir(dir);
-    }
-    else
-    {
-        KnapsackInstance ksInstance;
-        read_knapsack_file(path_instance, &ksInstance);
+    // Les fonctions qui suivent on besoin de -D pour fonctionner
+    return vns_gloutonne_vs_aleatoire(path_instance, temps_max); // Décommenter pour exécuter le test VNS Gloutonne vs Aléatoire sur un répertoire
 
-        ExperimentalResultsKSM results = run_all_experiments(&ksInstance, temps_max);
-        print_results_table(&results);
-    }
-
-    return 0;
 }
--- benchmark.h
+++ benchmark.h
@@ -12,11 +12,19 @@
 #include "heuristique.h"
 #include "genetic.h"
 
+#define SIZE_RESULT_BUFFER 1024 
 
 typedef struct {
-    double value;
-    double time;
-    int length;
+    char filename[256]; // vns
+    double value; // vns
+    double time; // vns 
+    int length; // vns
+    char* type ; // vns_aleatoire, vns_gloutonne, genetic, hybrid
+    int pop_size; // genetic
+    double mutation_rate; // genetic
+    int generations; // genetic
+    int vns_iterations; // hybrid
+    int k; // hybrid
 } ResultEntry;
 
 typedef struct {
@@ -41,10 +49,11 @@ double get_cpu_time();
  * @param initialization_function La fonction d'initialisation pour générer la solution initiale.
  *        Elle doit prendre un paramètre `KnapsackInstance *` et retourner un pointeur vers une solution de type `KnapsackSolution`.
  * @param temps_max Le temps maximum autorisé pour l'exécution de l'algorithme (en secondes).
+ * @param filename Le nom du fichier pour enregistrer les résultats.
  *
  * @return Les résultats expérimentaux, comprenant les valeurs de la solution et le temps CPU pour chaque étape.
  */
-ResultEntry run_experiment(const KnapsackInstance *instance, KnapsackSolution *(*initialization_function)(const KnapsackInstance *), int temps_max);
+ResultEntry run_experiment(const KnapsackInstance *instance, KnapsackSolution *(*initialization_function)(const KnapsackInstance *), int temps_max, const char *filename);
 
 
 /**
@@ -55,13 +64,14 @@ ResultEntry run_experiment(const KnapsackInstance *instance, KnapsackSolution *(
  * @param generations Le nombre de générations à exécuter.
  * @param mutation_rate Le taux de mutation pour l'algorithme génétique.
  * @param time_limit Le temps maximum autorisé pour l'exécution de l'algorithme (en secondes).
+ * @param filename Le nom du fichier pour enregistrer les résultats.
  * 
  * @return Les résultats de l'algorithme, comprenant la valeur de la solution, le temps CPU et la taille de la solution.
  * 
  * @note Cette fonction exécute l'algorithme génétique sur l'instance donnée et retourne les résultats sous forme de structure `ResultEntry`.
  *       Elle mesure le temps CPU écoulé pendant l'exécution et calcule la taille de la solution.
  */
-ResultEntry run_hybrid_algorithm(const KnapsackInstance *instance, int population_size, int generations, double mutation_rate, int vns_iterations, int k, int time_limit);
+ResultEntry run_hybrid_algorithm(const KnapsackInstance *instance, int population_size, int generations, double mutation_rate, int vns_iterations, int k, int time_limit, const char *filename);
 
 
 /**
@@ -72,23 +82,25 @@ ResultEntry run_hybrid_algorithm(const KnapsackInstance *instance, int populatio
  * @param generations Le nombre de générations à exécuter.
  * @param mutation_rate Le taux de mutation pour l'algorithme génétique.
  * @param time_limit Le temps maximum autorisé pour l'exécution de l'algorithme (en secondes).
+ * @param filename Le nom du fichier pour enregistrer les résultats.
  * 
  * @return Les résultats de l'algorithme, comprenant la valeur de la solution, le temps CPU et la taille de la solution.
  * 
  * @note Cette fonction exécute l'algorithme génétique sur l'instance donnée et retourne les résultats sous forme de structure `ResultEntry`.
  *      Elle mesure le temps CPU écoulé pendant l'exécution et calcule la taille de la solution.
  */
-ResultEntry run_genetic_algorithm(const KnapsackInstance *instance, int population_size, int generations, double mutation_rate, int time_limit) ;
+ResultEntry run_genetic_algorithm(const KnapsackInstance *instance, int population_size, int generations, double mutation_rate, int time_limit, const char *filename) ;
 
 /**
  * @brief Exécute toutes les expériences sur une instance de sac à dos donnée.
  *
  * @param instance L'instance du problème du sac à dos à résoudre.
  * @param temps_max Le temps maximum autorisé pour l'exécution de l'algorithme (en secondes).
+ * @param filename Le nom du fichier pour enregistrer les résultats.
  *
  * @return Les résultats expérimentaux, comprenant les valeurs de la solution et le temps CPU pour chaque étape.
  */
-ExperimentalResultsKSM run_all_experiments(const KnapsackInstance *instance, int temps_max);
+ExperimentalResultsKSM run_all_experiments(const KnapsackInstance *instance, int temps_max, const char *filename);
 
 /**
  * @brief Affiche les résultats des expérimentations sous forme de tableau.
@@ -97,5 +109,25 @@ ExperimentalResultsKSM run_all_experiments(const KnapsackInstance *instance, int
  */
 void print_results_table(const ExperimentalResultsKSM *results);
 
+
+/**
+ * @brief Exporte les résultats expérimentaux dans un fichier CSV.
+ *
+ * @param results Les résultats expérimentaux à exporter (de type `ResultEntry`).
+ * @param data_index L'index des données à exporter.
+ * @param filename Le nom du fichier CSV dans lequel les résultats seront exportés.
+ */
+void export_csv(ResultEntry *results, int data_index, const char *filename);
+
+/**
+ * @brief Exécute les algorithmes VNS Gloutonne et VNS Aléatoire sur un répertoire d'instances de sac à dos.
+ *
+ * @param repertoire Le chemin du répertoire contenant les fichiers d'instances.
+ * @param temps_max Le temps maximum autorisé pour l'exécution de l'algorithme (en secondes).
+ * 
+ * @note Cette fonction lit tous les fichiers d'instances dans le répertoire donné, exécute les algorithmes sur chaque instance,
+ * * et exporte les résultats dans un fichier CSV.
+ */
+int vns_gloutonne_vs_aleatoire(const char *repertoire, int temps_max);
 
 #endif // BENCHMARK_H
